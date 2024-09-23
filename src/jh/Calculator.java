abf9b17fdd077dccd448512674eb08042190d3ba
ljhee92 <weilainib7@gmail.com>
2024-09-23 12:01:01 +0900

    1일차: JUnit 기본 활용

---
+++ src/jh/Calculator.java
@@ -0,0 +1,26 @@
+package jh;
+
+public class Calculator {
+
+    public int plus(int a, int b) {
+//        int answer = 0;
+//
+//        if(a + b == 3) {
+//            answer = 3;
+//        } else {
+//            answer = 5;
+//        } // else
+//
+//        return answer;
+        return a + b;
+    } // plus
+
+    public int divide(int a, int b) { // 사실은 float가 더 적합..
+        return a / b;
+    } // divide
+
+    public int minus(int a, int b) {
+        return a - b;
+    } // minus
+
+} // class
